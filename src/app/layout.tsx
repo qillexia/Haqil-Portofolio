c6002d1aac333f0e251cb4ed0cb6e742ff024297
qillexia <haqilabdillah@gmail.com>
2026-07-06 00:08:32 +0700

    UX betterr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         {/* Load Material Symbols untuk Icon */}
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
